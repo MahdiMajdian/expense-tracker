f9464daae1fe07915ac1085edd4bea824ddc2218
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-10-22 20:09:49 +0330

    add header and add transaction button to page

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react"
+import Header from "../components/Header/Header"
+import Button from "../components/UI/Button"
+import { useAppSelector } from "../hooks"
 
 const Wallet = () => {
-    return (
-        <div>
-            Wallet
-        </div>
-    )
+	const balance = useAppSelector((state) => state.wallet.walletBalance)
+
+	return (
+		<div className="h-screen w-full flex justify-center items-center">
+			<div className="w-full max-w-lg h-full mt-32    ">
+				<Header
+					name="Wallet Balance"
+					date={Date.now()}
+					currencyType="USD"
+					total={balance}
+				/>
+				<div className="mt-8 w-full flex justify-end">
+					<Button className="">+ Add Transaction</Button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Wallet
