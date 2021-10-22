b810327d2917068597d32b5bf086f97a6b63af15
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-10-23 02:09:36 +0330

    add onClick and className to Card and Button component

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 interface IButtonProps {
 	className?: string
+	onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 const Button: React.FC<IButtonProps> = (props) => {
 	const { children, className } = props
 	return (
 		<button
-			className={`${className} bg-yellow-400 hover:bg-yellow-500 text-gray-800 font-medium rounded py-2 px-4`}>
+			onClick={props.onClick}
+			className={`${className} font-medium rounded py-2 px-4`}>
 			{children}
 		</button>
 	)
--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -1,10 +1,17 @@
 import React from "react"
 interface ICardProps {
 	className?: string
+	onClick?: React.MouseEventHandler<HTMLDivElement>
 }
 const Card: React.FC<ICardProps> = (props) => {
 	const { className, children } = props
-	return <div className={`rounded-md shadow-md hover:shadow-lg ${className}`}>{children}</div>
+	return (
+		<div
+			onClick={props.onClick}
+			className={`rounded-md shadow-md hover:shadow-lg ${className}`}>
+			{children}
+		</div>
+	)
 }
 
 export default Card
